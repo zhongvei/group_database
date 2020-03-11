abc61d3f4c981487ccc6751a33b1730f08514f72
zhongvei <zhongvei@gmail.com>
2020-03-11 14:41:27 +0800

    successfully migrate.py

--- models/user_activity.py
+++ models/user_activity.py
@@ -3,7 +3,7 @@ import peewee as pw
 from models.user import User
 from models.activity import Activity
 
-class User_Meal(BaseModel):
+class User_Activity(BaseModel):
 
     user = pw.ForeignKeyField(User, backref='meals')
     activity = pw.ForeignKeyField(Activity)
