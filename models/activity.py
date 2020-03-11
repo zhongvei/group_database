49d02ccf074242abb1044411f8a32ed7b540828a
zhongvei <zhongvei@gmail.com>
2020-03-11 14:42:23 +0800

    successfully migrate.py

--- models/activity.py
+++ models/activity.py
@@ -2,8 +2,9 @@ from models.base_model import BaseModel
 import peewee as pw
 
 class Activity(BaseModel):
+
     activity = pw.CharField(unique = True)
-    height = pw.IntegerField()
+    calories = pw.IntegerField()
 
     def validate():
         return
